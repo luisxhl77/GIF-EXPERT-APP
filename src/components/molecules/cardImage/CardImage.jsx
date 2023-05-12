6acc129ae41787f3077432ae5d26ee5da7d6816f
luis carlos hernandez lopez <luis.hernandezlo@pragma.com.co>
2023-05-12 08:50:00 -0500

    adding id in cards images

--- src/components/molecules/cardImage/CardImage.jsx
+++ src/components/molecules/cardImage/CardImage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const CardImage = ({image, title, id}) => {
   return (
-    <div className="card">
+    <div className="card" id={id}>
         <img src={image} alt={title} />
         <p>{title}</p>
     </div>
